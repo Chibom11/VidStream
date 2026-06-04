d4fab7f1d8449f281596f16146c4d76054dd5a9c
Shivam <shivamsurroach10@gmail.com>
2026-06-04 23:46:22 +0530

    prisma 7.8.0 bug fixed

--- upload-service/db/db.js
+++ upload-service/db/db.js
@@ -1,6 +1,13 @@
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from 'dotenv'
 
-const prisma=new PrismaClient()
+dotenv.config()
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
 
 export async function addVideoDetailsToDB(title,description,author,url){
     const videoData=await prisma.videoData.create({
